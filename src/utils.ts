430d31db4265fd1b403b5989587ac6092d6594fe
Jeff Schofield <jeff@jeffschofield.com>
2022-07-24 17:29:08 -0400

    feat: Add param to generateFractionUtil for using custom string generator function

--- src/utils.ts
+++ src/utils.ts
@@ -1,21 +1,21 @@
 export function generateUnitMultipleString(multiple:number) {
-  return `calc(${multiple} * var(--twgl-current-base))`
+	return `calc(${multiple} * var(--twgl-current-base))`
 }
 
 import numericQuantity from 'numeric-quantity'
-export function generateFractionUtil(fraction:string) {
-  let decimal = numericQuantity(fraction)
-  return { [fraction.replace(' ', '-')]: generateUnitMultipleString(decimal) }
+export function generateFractionUtil(fraction:string, generateString:(multiple:number)=>string = generateUnitMultipleString) {
+	let decimal = numericQuantity(fraction)
+	return { [fraction.replace(' ', '-')]: generateString(decimal) }
 }
 
 export function generateUnitConfig(fractions:string[]) {
-  let config = {}
-  for (let fraction of fractions) {
-    let decimal = numericQuantity(fraction)
-    Object.assign(config, { [fraction.replace(' ', '-')]: decimal })
-  }
+	let config = {}
+	for (let fraction of fractions) {
+		let decimal = numericQuantity(fraction)
+		Object.assign(config, { [fraction.replace(' ', '-')]: decimal })
+	}
 
-  return config
+	return config
 }
 
 interface RangeOptions {
