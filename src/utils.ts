944ba90259cae3261841534d107d6d3bf40feee9
Jeff Schofield <jeff@jeffschofield.com>
2023-03-19 15:02:14 -0400

    refactor: Allow a custom string generator to be passed to `generateRange`

--- src/utils.ts
+++ src/utils.ts
@@ -2,8 +2,10 @@ export function generateUnitMultipleString(multiple:number) {
 	return `calc(${multiple} * var(--twgl-current-base))`
 }
 
+type GenerateStringFn = typeof generateUnitMultipleString
+
 import numericQuantity from 'numeric-quantity'
-export function generateFractionUtil(fraction:string, generateString:(multiple:number)=>string = generateUnitMultipleString) {
+export function generateFractionUtil(fraction:string, generateString:GenerateStringFn = generateUnitMultipleString) {
 	let decimal = numericQuantity(fraction)
 	return { [fraction.replace(' ', '-')]: generateString(decimal) }
 }
@@ -21,16 +23,17 @@ export function generateUnitConfig(fractions:string[]) {
 interface RangeOptions {
 	denominators:number[]
 	inclusive?:boolean
+	generateString?:GenerateStringFn
 }
-export function generateRange(from:number, to:number, { denominators, inclusive }:RangeOptions = { denominators: [2, 4], inclusive: true }) {
+export function generateRange(from:number, to:number, { denominators, inclusive, generateString }:RangeOptions = { denominators: [2, 4], inclusive: true }) {
 	let range = {}
 
 	for (let i = from; i <= to; i++) {
-		if (i > 0 && (i != from || inclusive)) Object.assign(range, generateFractionUtil(`${i}`))
+		if (i > 0 && (i != from || inclusive)) Object.assign(range, generateFractionUtil(`${i}`, generateString))
 		if (i != to) {
 		for (let denominator of denominators) {
 				for (let numerator = 1; numerator < denominator; numerator++) {
-					Object.assign(range, generateFractionUtil(`${i > 0 ? i+' ' : ''}${numerator}/${denominator}`))
+					Object.assign(range, generateFractionUtil(`${i > 0 ? i+' ' : ''}${numerator}/${denominator}`, generateString))
 				}
 			}
 		}
